17e7292656d20e6d63987984ce8fe59c24d42c31
wataiPublic@gmail.com <wataiPublic@gmail.com>
2022-07-22 16:28:56 +0800

    111-0722-3

--- js/jW_test.js
+++ js/jW_test.js
@@ -3,6 +3,18 @@ $(function(){
         $(".coverBg,.jumpWindow").removeClass("display");
     })
 
+    //忘記密碼
+    $("header .password .forget_ic").click(function(){
+        $(".jumpWindow").removeClass("display");
+        $(".coverBg,.jumpWindow.forgotPw").addClass("display");
+    })
+
+    //請先登入
+    $(".unLogin").click(function(){
+        $(".jumpWindow").removeClass("display");
+        $(".coverBg,.jumpWindow.clickWindow.plsLogin").addClass("display");
+    })
+
     //最新公告
     $(".callNews").click(function(){
         $(".jumpWindow").removeClass("display");
